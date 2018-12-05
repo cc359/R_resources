00f4dff8dfd876ac2d5646176220e2f4f78cdadf
Chris Cooper <chrisjc1202@gmail.com>
2018-12-05 21:21:48 +0000

    Add files via upload

---
+++ Test_script.R
@@ -0,0 +1,14 @@
+setwd("C:/Users/Chris Cooper/Desktop/")
+
+DataSourceFilename = "Test_Data.csv"
+FirstLineIsColumnNames = TRUE
+Separator = ","
+
+DataFrame=RawDataFrame = read.table( file=DataSourceFilename,
+header=FirstLineIsColumnNames, sep=Separator,quote="\"", dec=".", 
+row.names=NULL, as.is=FALSE, na.strings="NA", colClasses=NA, nrows=0, skip=0,
+check.names=FALSE,fill=TRUE, strip.white=TRUE, blank.lines.skip=TRUE, 
+comment.char="",allowEscapes=FALSE, flush=FALSE,stringsAsFactors=TRUE,
+fileEncoding="", encoding="unknown")
+
+fix(DataFrame)
